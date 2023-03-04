80d5f76caf9e3a3a6abd89135a6dde26e26a5e68
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2023-03-04 23:05:15 +0700

    - [feat] transaction + unit test(exclude update ccount)

---
+++ db/sqlc/store.go
@@ -0,0 +1,90 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type Store struct {
+	db *sql.DB
+	q  *Queries
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+		q:  New(db),
+	}
+}
+
+func (s *Store) execTx(ctx context.Context, fn func(q *Queries) error) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx error: %v, rb error: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+
+}
+
+type TransferParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferResult struct {
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	Transfer    Transfer `json:"transfer"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+func (s *Store) TransferTx(ctx context.Context, arg TransferParams) (TransferResult, error) {
+	var result TransferResult
+
+	err := s.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		//Todo update account
+
+		return nil
+
+	})
+
+	return result, err
+}
